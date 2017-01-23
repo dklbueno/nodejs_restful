67e0fd1a31a3eb3c1290cd1e8458aa60f420930f
dklbueno <dkbueno@gmail.com>
2017-01-23 13:14:46 -0200

    Primeiro Commit

---
+++ app_config.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = module.exports = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+
+var allowCors = function(rec, res, next){
+	res.header('Access-Control-Allow-Origin','127.0.0.1:5000');
+	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers','Content-Type');
+	res.header('Access-Control-Allow-Credentials','true');
+	next();
+}
+
+app.listen(5000);
+
+app.use(allowCors);
